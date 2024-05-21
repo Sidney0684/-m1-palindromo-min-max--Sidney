5158f7ccd8acbe346440c6a2ab38eae08c9aa6ff
Sidney Lopes <sidney.silvalopes@gmail.com>
2024-05-21 00:05:39 -0300

    função Palindrome

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(string2) {
+    let nomeEmMinusculo = string2.toLowerCase();
+    let result = "";
+    let result2 = "";
+    let initialIndice = nomeEmMinusculo.length - 1;
+  
+    for (let id = 0; id < nomeEmMinusculo.length; id++) {
+      result += nomeEmMinusculo[id]; 
+       
+      console.log (result)
+  
+      for (let id = initialIndice; id >= 0; id--) 
+        result2 = nomeEmMinusculo[id] + result2
+  
+      console.log (result2)
+  
+        if (result == result2) {
+          return true;
+        }
+      }
+      return false;
+    }
+  
+  isPalindrome("Radar");
+  
+
+function arrayMaxMin(arr) {
+  /* Seu código aqui */
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+//let back = "";
+//let initialIndice = string2.length - 1;
+
+//for (let indice = initialIndice; indice >= 0; indice--) {
+//back = string2[indice] + back;
+
+//f ((back = initialIndice)) {
+//  return true;
+//  }
+//}
+//return false;

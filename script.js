6850ceed00264a616e37ac6b17afc6eda1e4b0ea
Sidney Lopes <sidney.silvalopes@gmail.com>
2024-05-21 20:22:47 -0300

    palindromo 2

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 function isPalindrome(string) {
-  let minusculo = string.toLowerCase().replace ();
+  let minusculo = string.toLowerCase().replace();
   let texto = string.length;
 
-  for (i = 0; i < texto ; i++) {
-    if (minusculo[i] !== minusculo[texto - 1 -i]) {
+  for (i = 0; i < texto; i++) {
+    if (minusculo[i] !== minusculo[texto - 1 - i]) {
       return false;
+    }
+    return true;
   }
-  return true;
-}
 }
 isPalindrome("Radar");
 
-function arrayMaxMin(arr) {
-  /* Seu código aqui */
+function arrayMaxMin(array) {
+  let menor = 0;
+  let maior = 0;
+
+  for (i = 0; i < array.length; i++) {}
 }
 
-//let back = "";
-//let initialIndice = string2.length - 1;
+//Requisitos
+//Para desenvolver essa tarefa você deve cumprir alguns requisitos:
 
-//for (let indice = initialIndice; indice >= 0; indice--) {
-//back = string2[indice] + back;
+//O nome da função deve ser arrayMaxMin;
+//A função deverá receber o array por parâmetro;
+//Sua função deve obrigatoriamente utilizar laço de repetição para percorrer e analisar o array recebido por parâmetro;
+//Sua função deve retornar um novo array onde no índice 0 será apresentado o valor mínimo e no índice 1 o valor máximo.
 
-//f ((back = initialIndice)) {
-//  return true;
-//  }
-//}
-//return false;
+//Dicas
+//Utilize variáveis auxiliares para armazenar, ao longo do looping, o maior e menor número encontrados até o momento;
+//Inicie suas variáveis auxiliares com o número 0, visto que os números dispostos dentro do array serão sempre
+//iguais ou maiores que 1. Ex. arr[n] >= 1;
+//Monte seu laço de repetição percorrendo todos os valores do array;
+//Monte o array de retorno com os valores armazenados nas variáveis auxiliares, mas deixe esse passo para ser
+//executado após o laço de repetição.
